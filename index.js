33d206ca9f740a2af4293b71aae16b4dd6929d80
sana-abbas <51811601+sana-abbas@users.noreply.github.com>
2019-08-27 09:15:52 +0500

    going live

--- index.js
+++ index.js
@@ -1,6 +1,81 @@
 // A $( document ).ready() block.
 // read more about this at https://learn.jquery.com/using-jquery-core/document-ready/
+const getTodos = () => {
+const hasuraUrl = 'https://leadwithher.herokuapp.com/v1/graphql';
+const postData = {
+query: `
+query getTodos {
+todos {
+id
+title
+completed
+}
+}
+`
+};
+
+$.ajax({
+type: 'POST',
+url: hasuraUrl,
+data: JSON.stringify(postData),
+contentType: 'application/json',
+dataType: 'json',
+success: (response) => {
+	const todos = response.data.todos;
+	console.log(todos);
+$('#todo-list').html(JSON.stringify(todos));
+},
+error: (error) => {
+console.log(error);
+}
+})
+}
+
+
 $( document ).ready(function() {
   console.log( "jQuery and the document is ready!" );
   $('#title').html('This was from JavaScript')
+
+  getTodos();
+
+  $('#submit').click(function(){
+    let newData = {
+	query: `
+	mutation insert_todos($newTodo:String!) {
+  	insert_todos(
+    objects: [
+      {
+        title: $newTodo,
+        completed: false
+      }
+    ]
+  	) {
+    returning {
+      id
+      title
+      completed
+    	}
+  	}
+	}
+	`,
+	variables: {
+		newTodo : $('#newTodo').val()
+	}
+}
+  $.ajax({
+  	type: "POST",
+  	url: 'https://leadwithher.herokuapp.com/v1/graphql',
+  	data: JSON.stringify(newData),
+  	contentType: 'application/json',
+    dataType: 'json',
+	success: (response) => {
+	const todos = response.data.todos;
+	console.log(todos);
+	$('#todo-list').html(JSON.stringify(todos));
+	},
+	error: (error) => {
+	console.log(error);
+	}
+  })
+})
 });
